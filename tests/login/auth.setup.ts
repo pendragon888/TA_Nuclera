6e057e29ec7e93a9e5ceb922661fcb5dd7756f6e
pendragon888 <pendragon888@hotmail.com>
2026-04-02 00:31:55 +0100

    Try path.join to resolve 'Error: ENOENT: no such file or directory' on github

--- tests/login/auth.setup.ts
+++ tests/login/auth.setup.ts
@@ -1,15 +1,16 @@
 import { test as setup} from '@fixtures/pages.fixture'
+import path from 'path'
 
 setup.describe('Setting up UI storage state files', async () => {
   setup("Login as Admin", async ({ page, loginPage }) => {
-    const authFile = ('playwright/.auth/adminAuth.json');
+    const authFile = path.join(__dirname, '../../playwright/.auth/adminAuth.json');
     await loginPage.login(process.env.ADMIN_USERNAME!, process.env.ADMIN_PASSWORD!)
     await page.waitForURL(process.env.BASE_URL!+'/projects');
     await page.context().storageState({ path: authFile });
   });
 
   setup("Login as NonAdmin", async ({ page, loginPage }) => {
-    const userFile = "playwright/.auth/userAuth.json";
+    const userFile = path.join(__dirname, '../../playwright/.auth/userAuth.json');
     await loginPage.login(process.env.NONADMIN_USERNAME!, process.env.NONADMIN_PASSWORD!)
     await page.waitForURL(process.env.BASE_URL!+'/projects');
     await page.context().storageState({ path: userFile });
